923e18e4a3e0ed69fe97e63c534f02f9125d8f40
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2021-05-27 21:06:11 -0400

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,25 @@
 #include <iostream>
 #include <iomanip>
 #include <filesystem>
+#include <utility>
 #include "ahnen.h"
-        
-/*
- TODO: Add padding to mens and womens names based respectively on the longest man's name and the longest woman's name. To padd on the right, left justify the names by setting the alignment.
-*/
+ 
+std::pair<int, int> get_name_widths(const std::vector<Ahnen>& vec)
+{
+   auto f_max = 0; 
+   auto m_max = 0;
+   for (auto&& x : vec) {
+
+          if (0 == stoi(x.number()) % 2)
+             f_max = (x.name().size() > f_max) ? x.name().size() : f_max;
+          else 
+             m_max = (x.name().size() > m_max) ? x.name().size() : m_max;
+   }
+   return {f_max, m_max};
+}
 
 using namespace std;
 namespace fs = std::filesystem;
-
 int main(int argc, char** argv) 
 {
     if (argc != 2) {
@@ -32,8 +42,10 @@ int main(int argc, char** argv)
 
     ofstream ofstr("./data/output.txt");
 
-     auto anum_width = vec[vec.size() - 1].number().size();
+    auto anum_width = vec[vec.size() - 1].number().size();
 
+    auto [father_width, mother_width] = get_name_widths(vec);
+    
     for (auto i = 0; i < vec.size();) {
        
        auto father_anum = stoi(vec[i].number());
@@ -42,13 +54,13 @@ int main(int argc, char** argv)
 
       // Does a mother exist for this father?
        // The next entry is even, a man, so we create an "Unknown" wife.
-       ofstr << setw(anum_width) << setfill('0') << father_anum << '_' << setw(anum_width) << setfill('0') << father_anum + 1 << " [" << vec[i].name() << " ]  [";
+       ofstr << std::right << setw(anum_width) << setfill('0') << father_anum << '_' << setw(anum_width) << setfill('0') << father_anum + 1 << " [" << vec[i].name() << " ]  [";
  
        auto mother_index = i + 1;
        
        if (stoi(vec[mother_index].number()) != father_anum + 1) {
            
-             ofstr << "Unknown";
+             ofstr <<  "Unknown";
              
              ++i;
              
@@ -59,7 +71,37 @@ int main(int argc, char** argv)
           i+=2;
        }
        ofstr << " ]\n";
-    }   
+       
+    }  
+    /*
+    for (auto i = 0; i < vec.size();) {
+       
+       auto father_anum = stoi(vec[i].number());
+
+       auto remainder = father_anum % 2;
+
+      // Does a mother exist for this father?
+       // The next entry is even, a man, so we create an "Unknown" wife.
+       ofstr << std::right << setw(anum_width) << setfill('0') << father_anum << '_' << setw(anum_width) << setfill('0') << father_anum + 1 << " [" << setw(father_width) << std::left << setfill(' ') << vec[i].name() << " ]  [";
+ 
+       auto mother_index = i + 1;
+       
+       if (stoi(vec[mother_index].number()) != father_anum + 1) {
+           
+             ofstr << setw(mother_width) << setfill(' ') << std::left << "Unknown";
+             
+             ++i;
+             
+       } else {
+           
+          ofstr  << setw(mother_width) << setfill(' ') << std::left << vec[mother_index].name();
+          
+          i+=2;
+       }
+       ofstr << " ]\n";
+       
+    }  
+     */  
     return 0;
 }
 
